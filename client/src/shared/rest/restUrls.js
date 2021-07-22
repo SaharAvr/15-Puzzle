62f04ca4ea48969f2630d17715fc8229d3367dcb
Sahar Avr <sahara@welldone-software.com>
2021-07-22 15:52:37 +0300

    [FIX] rest - fix API base url for development env

--- client/src/shared/rest/restUrls.js
+++ client/src/shared/rest/restUrls.js
@@ -1,13 +1,23 @@
 import _ from 'lodash';
 
+const apiBase = (() => {
+
+  if (process.env.NODE_ENV === 'development') {
+    return `${window.location.protocol}//${window.location.hostname}:${process.env.REACT_APP_LOCAL_SERVER_PORT}${process.env.REACT_APP_API_BASE}`;
+  }
+
+  return process.env.REACT_APP_API_BASE;
+  
+})();
+
 const endpoints = {
   CONFIGURATION: '/configuration',
   RECORD: '/record',
   LEADERBOARD: '/leaderboard',
 };
 
-const restUrls = _.reduce(endpoints, (res, value, key) => {
-  res[key] = `${process.env.REACT_APP_API_BASE}${value}`;
+const restUrls = _.reduce(endpoints, (res, endpoint, endpointKey) => {
+  res[endpointKey] = `${apiBase}${endpoint}`;
   return res;
 }, {});
 

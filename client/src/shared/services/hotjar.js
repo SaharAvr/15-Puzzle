1a633e948f24f8bffc315ec6206b714f39f405f2
Sahar Avr <sahara@welldone-software.com>
2021-08-01 23:58:40 +0300

    [FIX] services - hotjar - init script through service

--- client/src/shared/services/hotjar.js
+++ client/src/shared/services/hotjar.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 class HotjarService {
 
   constructor() {
@@ -8,10 +6,28 @@ class HotjarService {
 
   init() {
 
-    if (!_.isFunction(window.hj)) {
+    if (this.instance) {
       return;
     }
 
+    if (window.hj) {
+      this.instance = window.hj;
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.innerHTML = `
+      (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2530463,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    `;
+
+    document.head.appendChild(script);
     this.instance = window.hj;
 
   }

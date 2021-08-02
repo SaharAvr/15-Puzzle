891b8e4731924e9f0810e736560b4585dab281ee
Sahar Avr <sahara@welldone-software.com>
2021-08-02 15:29:29 +0300

    [FEAT] scripts - add deploy script

---
+++ deploy.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-console */
+
+const path = require('path');
+const fs = require('fs-extra');
+
+const rootPath = path.join.bind(path, __dirname);
+
+const dirPaths = {
+  ROOT: rootPath,
+  CLIENT: rootPath('./client'),
+  BUILD: rootPath('./client/build'),
+};
+
+const spawnPromise = (cmd, args, options) => {
+
+  // eslint-disable-next-line global-require
+  const { spawn } = require('child_process');
+
+  return new Promise((resolve, reject) => {
+
+    const process = spawn(cmd, args, options);
+
+    process.stdout.on('data', data => console.log(`${data}`));
+    process.stderr.on('data', data => console.log(`${data}`));
+    process.on('close', resolve);
+    process.on('error', reject);
+
+  });
+  
+};
+
+const deleteBuildFolder = () => (
+    
+  fs.rmSync(dirPaths.BUILD, { recursive: true })
+
+);
+
+const buildProject = () => (
+
+  spawnPromise('yarn', ['build'], {
+    cwd: dirPaths.CLIENT,
+    shell: true,
+  })
+
+);
+
+const deploy = () => (
+
+  spawnPromise('git', ['push -f heroku'], {
+    cwd: dirPaths.CLIENT,
+    shell: true,
+  })
+
+);
+
+(async () => {
+
+    deleteBuildFolder();
+    await buildProject();
+    await deploy();
+    deleteBuildFolder();
+
+})();

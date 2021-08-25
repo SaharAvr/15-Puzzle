d89d5445266989c44970a35dbac2782b826ce43a
Sahar Avr <sahara@welldone-software.com>
2021-08-25 11:06:42 +0300

    [REFACTOR] services - hotjar & analytics - move script to index.html

--- client/src/shared/services/analytics.js
+++ client/src/shared/services/analytics.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 class AnalyticsService {
 
   constructor() {
@@ -6,32 +8,11 @@ class AnalyticsService {
 
   init() {
 
-    if (this.instance) {
+    if (!_.has(window.dataLayer)) {
       return;
     }
 
-    if (window.dataLayer) {
-      this.instance = window.dataLater;
-      return;
-    }
-    
-    const dataLayerScript = document.createElement('script');
-    dataLayerScript.innerHTML = `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-5RMBP9Q75D');
-    `;
-
-    const gtagScript = document.createElement('script');
-    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-5RMBP9Q75D';
-    gtagScript.async = true;
-    gtagScript.onload = () => {
-      document.head.appendChild(dataLayerScript);
-      this.instace = window.dataLater;
-    };
-
-    document.head.appendChild(gtagScript);
+    this.instance = window.dataLayer;
 
   }
 
--- client/src/shared/services/hotjar.js
+++ client/src/shared/services/hotjar.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 class HotjarService {
 
   constructor() {
@@ -6,28 +8,10 @@ class HotjarService {
 
   init() {
 
-    if (this.instance) {
+    if (!_.isFunction(window.hj)) {
       return;
     }
 
-    if (window.hj) {
-      this.instance = window.hj;
-      return;
-    }
-
-    const script = document.createElement('script');
-    script.innerHTML = `
-      (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:2530463,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    `;
-
-    document.head.appendChild(script);
     this.instance = window.hj;
 
   }
